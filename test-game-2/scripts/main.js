bc4584ba0b7b9b1136d8a83c7aa7c7302e31ccc1
Anton Kormishenkov <easy100@yandex.ru>
2023-02-16 01:27:29 +0400

    WIP

--- test-game-2/scripts/main.js
+++ test-game-2/scripts/main.js
@@ -4,10 +4,9 @@ const ASSETS = {
 	'middle'	 : 'assets/middle.png',
 	'player'	 : 'assets/player.png',
 	'tileset'	 : 'assets/tileset.png',
-	'tileset_2'	 : 'assets/tileset_2.png'
+	'tileset_2'	 : 'assets/tileset_2.png',
+	'map_0'	 	 : 'assets/maps/0.json'
 };
-
-console.log(ASSETS)
 
 resource.init(ASSETS, () => {
 
--- test-game-2/scripts/resource.js
+++ test-game-2/scripts/resource.js
@@ -1,5 +1,38 @@
 let resource = {};
 
-resource.init = function(array, callback) {
+resource.init = function(assets, callback) {
+	let loader = PIXI.Loader.shared;
 
+	for(let key in assets) {
+		let path = assets[key];
+		loader.add(key, path)
+	}
+
+	loader.load(callback);
+};
+
+resource.getTexture = function(key) {
+	let loader = PIXI.Loader.shared;
+
+	if(key in loader.resources) {
+		let rsrc = loader.resources[key];
+		if(rsrc.type === PIXI.LoaderResource.TYPE.IMAGE) {
+			return rsrc.texture;
+		}
+	}
+
+	return false;
+};
+
+resource.getJSON = function(key) {
+	let loader = PIXI.Loader.shared;
+
+	if(key in loader.resources) {
+		let rsrc = loader.resources[key];
+		if(rsrc.type === PIXI.LoaderResource.TYPE.JSON) {
+			return rsrc.data;
+		}
+	}
+
+	return false;
 };
