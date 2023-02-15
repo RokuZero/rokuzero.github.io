391619e88d2f0798f4d99b5526628f59aad59fda
Anton Kormishenkov <easy100@yandex.ru>
2023-02-16 01:12:31 +0400

    WIP

---
+++ test-game-2/scripts/main.js
@@ -0,0 +1,14 @@
+const ASSETS = {
+	'background' : 'assets/background.png',
+	'collision'  : 'assets/collision.png',
+	'middle'	 : 'assets/middle.png',
+	'player'	 : 'assets/player.png',
+	'tileset'	 : 'assets/tileset.png',
+	'tileset_2'	 : 'assets/tileset_2.png'
+};
+
+console.log(ASSETS)
+
+resource.init(ASSETS, () => {
+
+});
---
+++ test-game-2/scripts/resource.js
@@ -0,0 +1,5 @@
+let resource = {};
+
+resource.init = function(array, callback) {
+
+};
